001e08af4cc6d2d945f79ba948ef0d52a040fd38
Brendaar13 <138994387+Brendaar13@users.noreply.github.com>
2024-03-18 17:21:31 +0100

    Create ex01 - ft_putstr.c

---
+++ ex01 - ft_putstr.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		write(1, &str[a], 1);
+		a++;
+	}
+}
+/*int main()
+{
+	ft_putstr("aiua");
+}*/
